57b839e9aae66b119630764348a28891323d0e32
TheGreyBull <thegreybull@outlook.com>
2023-12-13 23:18:42 +0100

    a

--- src/ClientApp.java
+++ src/ClientApp.java
@@ -1,2 +1,43 @@
-public class ClientApp {
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class ClientApp extends JFrame {
+
+    private String serverCommand;
+    private final int PORT = 5555;
+
+    protected ClientApp() {
+        this.setSize(800, 500);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            Socket clientSocket = new Socket("192.168.1.1", 5555);
+
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            inFromUser.readLine();
+
+            outToServer.writeBytes("abc");
+
+            inFromServer.readLine();
+
+            clientSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createApp() {
+
+    }
 }
--- src/ServerApp.java
+++ src/ServerApp.java
@@ -1,2 +1,41 @@
-public class ServerApp {
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp extends JFrame {
+
+    private String clientCommand;
+    private ServerSocket welcomeSocket;
+    private final int PORT = 5555;
+
+    protected ServerApp() {
+        this.setSize(800, 500);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //
+        while (true) {
+            try {
+                Socket connectionSocket = welcomeSocket.accept();
+
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+                inFromClient.readLine();
+
+                outToClient.writeBytes("abc");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void createApp() {
+
+    }
 }
